40f63251d45e162c247960fe7c3b0f7b53f80fe3
LucasRomanato0 <lucasromanato34@gmail.com>
2024-03-18 18:33:32 -0300

    adicionando chromadb

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -6,6 +6,7 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
+from langchain_core.output_parsers import StrOutputParser
 
 dotenv.load_dotenv()
 
@@ -41,4 +42,6 @@ review_prompt_template = ChatPromptTemplate(
 
 chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
-review_chain = review_prompt_template | chat_model
+output_parser = StrOutputParser() # fromata a responsta
+
+review_chain = review_prompt_template | chat_model | output_parser
